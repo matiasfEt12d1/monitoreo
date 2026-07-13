d83341b663083a3955f589f5b64c8a56999137d1
matiasfEt12d1 <matias.floret12d1@gmail.com>
2026-07-13 10:15:36 -0300

    update db script

--- scripts/tables/database.sql
+++ scripts/tables/database.sql
@@ -8,7 +8,7 @@ USE monitoreo;
 CREATE TABLE Laboratorio (
     id_laboratorio INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
-    ubicacion VARCHAR(250)
+    ubicacion VARCHAR(250) NOT NULL
 );
 
 CREATE TABLE ModelosPC (
@@ -23,23 +23,25 @@ CREATE TABLE Computadoras (
     idPC INT PRIMARY KEY AUTO_INCREMENT,
     codigoInventario VARCHAR(45) UNIQUE NOT NULL,
     numero_serie VARCHAR(50) UNIQUE NOT NULL,
-    id_laboratorio INT,
-    idModelo INT,
+    id_laboratorio INT NOT NULL,
+    idModelo INT NOT NULL,
     sistemaOperativo VARCHAR(50),
+    tipoPC ENUM('Escritorio', 'Servidor') NOT NULL,
+    max_temperatura_tolerada DECIMAL(5,2) NULL,
     CONSTRAINT fk_pc_lab FOREIGN KEY (id_laboratorio) REFERENCES Laboratorio(id_laboratorio),
     CONSTRAINT fk_pc_modelo FOREIGN KEY (idModelo) REFERENCES ModelosPC(idModelo)
 );
 
 CREATE TABLE Mediciones (
     idMedicion INT PRIMARY KEY AUTO_INCREMENT,
-    idPC INT,
-    macAddress VARCHAR(45) UNIQUE,
-    fechaIngreso DATETIME,
-    fechaArreglo DATETIME,
-    fechaDesecho DATETIME,
-    estado VARCHAR(25) DEFAULT 'Operativo',
-    temperaturaCPU DECIMAL(5, 2),
-    usoRAM DECIMAL(5, 2),
+    idPC INT NOT NULL,
+    macAddress VARCHAR(45),
+    fechaIngreso DATETIME NOT NULL DEFAULT NOW(),
+    fechaArreglo DATETIME NULL,
+    fechaDesecho DATETIME NULL,
+    estado VARCHAR(25) NOT NULL DEFAULT 'Operativo',
+    temperaturaCPU DECIMAL(5, 2) NOT NULL,
+    usoRAM DECIMAL(5, 2) NOT NULL,
     CONSTRAINT fk_medicion_computadora 
         FOREIGN KEY (idPC) REFERENCES Computadoras(idPC)
         ON DELETE CASCADE
