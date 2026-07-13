becf97ef9e8c2e5f7f8112ee0f6d951c914d9b1d
matiasfEt12d1 <matias.floret12d1@gmail.com>
2026-07-13 11:12:03 -0300

    reset program to initial structure

--- src/presentation/Program.cs
+++ src/presentation/Program.cs
@@ -15,19 +15,6 @@ namespace presentation
             {
                 // Creamos la base de datos y las tablas si no existen
                 Repositorio.InicializarBaseDeDatos();
-
-                // Llamamos al repositorio que está en la capa 'data'
-                List<Computadora> computadoras = Repositorio.ObtenerTodas();
-
-                foreach (var pc in computadoras)
-                {
-                    // Usamos las entidades y el polimorfismo de la capa 'domain'
-                    pc.AgregarMedicion(new Medicion("00:1A:2B:3C:4D:5E", 75.5m, 88.0m));
-
-                    Console.WriteLine($"PC: {pc.CodigoInventario} | Tipo: {pc.GetType().Name}");
-                    Console.WriteLine($"¿Crítico?: {(pc.EvaluarEstadoCritico() ? "SI" : "NO")}");
-                    Console.WriteLine(new string('-', 30));
-                }
             }
             catch (Exception ex)
             {
@@ -39,6 +26,12 @@ namespace presentation
                     Console.WriteLine($"\nCausa real (InnerException): {ex.InnerException.Message}");
                     Console.WriteLine($"Rastreo (StackTrace):\n{ex.InnerException.StackTrace}");
                 }
+            }
+            finally
+            {
+                // Esto mantendrá la consola abierta para que leas el error
+                Console.WriteLine("\nPresiona Enter para salir...");
+                Console.ReadLine();
             }
         }
     }
